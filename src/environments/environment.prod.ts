b70edd028f18b93bf08bbd597b0d31f9ea0c4e56
antonio.neto <antonio.neto@dsr-corporation.com>
2020-08-11 15:54:12 +0100

    update static spa site

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,11 +3,11 @@ export const environment = {
   identityServer: {
     authority: 'https://identityserver-auth.azurewebsites.net',
     clientId: 'angular_spa',
-    redirectUri: 'https://ambitious-beach-01ab44003.azurestaticapps.net/auth-callback',
-    postLogoutRedirectUri: 'https://ambitious-beach-01ab44003.azurestaticapps.net/',
+    redirectUri: 'https://identityserverspa.z33.web.core.windows.net/auth-callback',
+    postLogoutRedirectUri: 'https://identityserverspa.z33.web.core.windows.net/',
     responseType: 'id_token token',
     scope: 'openid profile email api.read',
-    silentRedirectUri: 'https://ambitious-beach-01ab44003.azurestaticapps.net/silent-refresh.html',
+    silentRedirectUri: 'https://identityserverspa.z33.web.core.windows.net/silent-refresh.html',
     resourceApiUri: 'http://identityserver-resource.azurewebsites.net'
   }
 };
